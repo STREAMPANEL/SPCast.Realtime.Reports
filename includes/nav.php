4624098cef246f558fee54c9a19c689bd4954cf4
Sven <scysys@outlook.com>
2024-11-30 12:42:16 +0100

    Create nav.php

---
+++ includes/nav.php
@@ -0,0 +1,51 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="https://live.spcast.eu/">SPCast Live</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="https://live.spcast.eu/">Startseite</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="all.php">Eingehend & Ausgehend</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="in.php">Eingehend</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="out.php">Ausgehend</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="country.php">Land</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="region.php">Bundesland</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="city.php">Stadt</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="os.php">Betriebssystem</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="browser.php">Browser</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="mount.php">Mountpoint</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="protocol.php">Protokoll</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="port.php">Port</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="ipversion.php">IP-Version</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
