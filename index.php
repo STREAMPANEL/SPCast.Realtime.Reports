748553d7d02d386afd58d26c4765264d722a8b10
Sven <scysys@outlook.com>
2024-11-30 13:00:41 +0100

    load local for users privacy

--- index.php
+++ index.php
@@ -9,8 +9,10 @@
     <meta name="keywords" content="spcast live, spcast echtzeitstatistiken, nutzerstatistik, netzwerkstatistiken, live daten, sendernutzung">
     <link rel="canonical" href="https://live.spcast.eu/" />
 
-    <!-- Include CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Bootstrap CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <!-- Bootstrap ICONS -->
+    <link rel="stylesheet" href="css/bootstrap-icons.min.css">
 </head>
 
 <body>
