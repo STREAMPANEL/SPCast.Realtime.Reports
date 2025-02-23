d05d20085921b6385c4463f58b37b78b6beef3c8
Sven <scysys@outlook.com>
2025-02-23 15:52:23 +0100

    better spacing

--- includes/footer.php
+++ includes/footer.php
@@ -2,27 +2,23 @@
 <script src="js/popper.min.js"></script>
 <script src="js/bootstrap.min.js"></script>
 
+<!-- Footer -->
 <footer class="bg-light text-center text-lg-start mt-5">
     <div class="container p-4">
-        </div>
-<div class="row">
-            <div class="col-lg-12 col-md-12 mb-4 mb-md-0">
-                <ul class="list-inline d-flex justify-content-center">
-                    <li class="list-inline-item">
-                        <a href="https://www.spcast.eu/kontakt/impressum/" target="_blank" rel="noopener" class="text-dark text-decoration-none">Impressum</a> |
-                    </li>
-                    <li class="list-inline-item">
-                        <a href="https://www.spcast.eu/kontakt/datenschutz/" target="_blank" rel="noopener" class="text-dark text-decoration-none">Datenschutz</a> |
-                    </li>
-                    <li class="list-inline-item">
-                        <a href="https://www.spcast.eu/kontakt/cookies/" target="_blank" rel="noopener" class="text-dark text-decoration-none">Cookies</a> |
-                    </li>
-                    <li class="list-inline-item">
-                        <a href="https://github.com/STREAMPANEL/SPCast.Realtime.Reports" target="_blank" rel="noopener" class="text-dark text-decoration-none">Github</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
+        <ul class="list-inline d-flex justify-content-center">
+            <li class="list-inline-item">
+                <a href="https://www.spcast.eu/kontakt/impressum/" target="_blank" rel="noopener" class="text-dark text-decoration-none">Impressum</a> |
+            </li>
+            <li class="list-inline-item">
+                <a href="https://www.spcast.eu/kontakt/datenschutz/" target="_blank" rel="noopener" class="text-dark text-decoration-none">Datenschutz</a> |
+            </li>
+            <li class="list-inline-item">
+                <a href="https://www.spcast.eu/kontakt/cookies/" target="_blank" rel="noopener" class="text-dark text-decoration-none">Cookies</a> |
+            </li>
+            <li class="list-inline-item">
+                <a href="https://github.com/STREAMPANEL/SPCast.Realtime.Reports" target="_blank" rel="noopener" class="text-dark text-decoration-none">Github</a>
+            </li>
+        </ul>
     </div>
     <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
         &copy; <?php echo date("Y"); ?> SPCast.eu
