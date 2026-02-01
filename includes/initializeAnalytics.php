63c32c0bf2d4ed6658cecb18d3add4c54dd3dc08
Sven <scysys@outlook.com>
2026-02-02 00:27:34 +0100

    Revert "security fixes and stability improvements"
    This reverts commit 5686e16ef03182024756ceca58a85aecc24af461.

--- includes/initializeAnalytics.php
+++ includes/initializeAnalytics.php
@@ -2,15 +2,10 @@
 
 function initializeAnalytics()
 {
-    try {
-        $client = new Google_Client();
-        $client->setAuthConfig('../service-account.json');
-        $client->addScope(Google_Service_AnalyticsData::ANALYTICS);
-        
-        return new Google_Service_AnalyticsData($client);
-    } catch (Exception $e) {
-        error_log('Analytics initialization failed: ' . $e->getMessage());
-        throw $e;
-    }
+
+    $client = new Google_Client();
+    $client->setAuthConfig('../service-account.json');
+    $client->addScope(Google_Service_AnalyticsData::ANALYTICS);
+    return new Google_Service_AnalyticsData($client);
 }
 
